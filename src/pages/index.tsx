b03667bac365cae9f519efa04a1c5b358ed3f0f7
Emily Eros <emily.eros@gmail.com>
2019-12-17 23:18:27 -0800

    configure maxStay tab + activity styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,29 +26,59 @@ var mapboxAccessToken= 'pk.eyJ1Ijoic2FhZGlxbSIsImEiOiJjamJpMXcxa3AyMG9zMzNyNmdxN
 //loads map style
 const defaultMapStyle = fromJS(mapStyle)
 
+//sunset
+// const MAXSTAY_COLOR_MAP:{ [key: string]: any } = {
+//     "3": "#FFDF00",
+//     "15": "#F1B408",
+//     "30": "#F1871C",
+//     "60": "#F06121",
+//     "120": "#F12627",
+//     "180": "#C80286",
+//     "240": "#63238A",
+// }
+
+//opposite of sunset
+// const MAXSTAY_COLOR_MAP:{ [key: string]: any } = {
+//     "3": "#FFDF00",
+//     "15": "#8BBA25",
+//     "30": "#018D5A",
+//     "60": "#00A8C4",
+//     "120": "#1078C3",
+//     "180": "#4336A2",
+//     "240": "#6D238A",
+// }
+
+//blues
 const MAXSTAY_COLOR_MAP:{ [key: string]: any } = {
-    "3": "#f7fcf5",
-    "15": "#f7fcf5",
-    "30": "#e5f5e0",
-    "45": "#c7e9c0",
-    "60": "#a1d99b",
-    "90": "#74c476",
-    "120": "#41ab5d",
-    "180": "#238b45",
-    "240": "#006d2c",
-    "360": "#00441b",
-    "480": "#00441b"
+    "3": "#e1f5fe",
+    "15": "#81d4fa",
+    "30": "#4fc3f7",
+    "60": "#03a9f4",
+    "120": "#0277bd",
+    "180": "#01579b",
+    "240": "#00345D",
 }
 
+//greens
+// const MAXSTAY_COLOR_MAP:{ [key: string]: any } = {
+//     "3": "#ffee58",
+//     "15": "#cddc39",
+//     "30": "#7cb342",
+//     "60": "#689f38",
+//     "120": "#388e3c",
+//     "180": "#1b5e20",
+//     "240": "#124116",
+// }
+
 const ACTIVITY_COLOR_MAP = {
-    "no stopping":"#000000",
-    "no parking":"#FF3D00",
-    "passenger loading":"#FFAE00",
+    "no stopping":"#777777",
+    "no parking":"#DD2C00",
+    "passenger loading":"#FF9100",
     "loading":"#FFEA00",
     "transit":"#37B34A",
     "free parking":"#00E5FF",
     "paid parking":"#2979FF",
-    "restricted":"#9A3AC9"
+    "restricted":"#AA00FF"
 };
 
 const scaledOffset = (offset:number) => {return {
@@ -189,7 +219,7 @@ class Map extends React.Component<PageProps, {}> {
     state= {
       mode: "activity",
       day: "mo",
-      time: "08:00",
+      time: "08:01",
       mapStyle: defaultMapStyle,
       viewport: {
         width: '100vw',
@@ -228,7 +258,9 @@ class Map extends React.Component<PageProps, {}> {
       const map = this._getMap();
 
       if(map) {
-        map.on('click', 'places', function (e) {
+        // TODO doesn't fire due to overlays div
+        map.on('mouseover', 'dataLayer', function (e) {
+          console.log({e})
           var coordinates = e.features[0].geometry.coordinates.slice();
           var description = e.features[0].properties.description;
 
@@ -239,10 +271,11 @@ class Map extends React.Component<PageProps, {}> {
           coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
           }
 
-          new mapboxgl.Popup()
-          .setLngLat(coordinates)
-          .setHTML(description)
-          .addTo(map);
+          // TODO needs work
+          // new mapboxgl.Popup()
+          // .setLngLat(coordinates)
+          // .setHTML(description)
+          // .addTo(map);
           });
       }
 
@@ -303,22 +336,6 @@ class Map extends React.Component<PageProps, {}> {
     render() {
       const {viewport, mapStyle, day, time, mode} = this.state;
 
-      var legend;
-
-      if(mode === "maxStay") {
-        legend = (<div style={{"margin": "15px"}}>
-          <b>Max Stay (minutes)</b>:<br/>
-          <Badge color="#f7fcf5" text="15 min" /> &nbsp;
-          <Badge color="#e5f5e0" text="30 min" /> &nbsp;
-          <Badge color="#c7e9c0" text="45 min" /> &nbsp;
-          <Badge color="#a1d99b" text="60 min" />&nbsp;
-          <Badge color="#74c476" text="90 min" />&nbsp;
-          <Badge color="#41ab5d" text="120 min" />&nbsp;
-          <Badge color="#238b45" text="180 min" />&nbsp;
-          <Badge color="#006d2c" text="240 min" />&nbsp;
-        </div>)
-      }
-
 
 // shows everything. would be great if this could intersect the feature collection with the viewport bounding box. i can't figure it out. for kevin?
       const features = filterCurblrData(this.props.curblr.data, this.state.day, this.state.time, this.state.mode)
@@ -328,28 +345,25 @@ class Map extends React.Component<PageProps, {}> {
           "no parking": features.features.filter(f => f.properties.activity === 'no parking').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
           "passenger loading": features.features.filter(f => f.properties.activity === 'passenger loading').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
           "loading": features.features.filter(f => f.properties.activity === 'loading').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-//          "taxi stand": features.features.filter(f => f.properties.activity === 'taxi stand').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
           "free parking": features.features.filter(f => f.properties.activity === 'free parking').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
           "transit": features.features.filter(f => f.properties.activity === 'transit').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
           "paid parking": features.features.filter(f => f.properties.activity === 'paid parking').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
           "restricted": features.features.filter(f => f.properties.activity === 'restricted').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          // "bike/scooter parking": features.features.filter(f => f.properties.activity === 'bike/scooter parking').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
       };
 
       const MAXSTAY_LENGTH_CALC = {
-          "3": features.features.filter(f => f.properties.maxStay === '3').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "15": features.features.filter(f => f.properties.maxStay === '15').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "30": features.features.filter(f => f.properties.maxStay === '30').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "45": features.features.filter(f => f.properties.maxStay === '45').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "60": features.features.filter(f => f.properties.maxStay === '60').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "90": features.features.filter(f => f.properties.maxStay === '90').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "120": features.features.filter(f => f.properties.maxStay === '120').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "180": features.features.filter(f => f.properties.maxStay === '180').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "240": features.features.filter(f => f.properties.maxStay === '240').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "360": features.features.filter(f => f.properties.maxStay === '360').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
-          "480": features.features.filter(f => f.properties.maxStay === '480').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
+          "3": features.features.filter(f => Number(f.properties.maxStay) <= 5).map(f => Number(f.properties.length)).reduce((acc, x) => acc + x, 0),
+          "15": features.features.filter(f => Number(f.properties.maxStay) === 15).map(f => Number(f.properties.length)).reduce((acc, x) => acc + x, 0),
+          "30": features.features.filter(f => Number(f.properties.maxStay) === 30).map(f => Number(f.properties.length)).reduce((acc, x) => acc + x, 0),
+          //"45": features.features.filter(f => f.properties.maxStay === '45').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
+          "60": features.features.filter(f => Number(f.properties.maxStay) === 60).map(f => Number(f.properties.length)).reduce((acc, x) => acc + x, 0),
+          //"90": features.features.filter(f => f.properties.maxStay === '90').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
+          "120": features.features.filter(f => Number(f.properties.maxStay) === 120).map(f => Number(f.properties.length)).reduce((acc, x) => acc + x, 0),
+          "180": features.features.filter(f => Number(f.properties.maxStay) === 180).map(f => Number(f.properties.length)).reduce((acc, x) => acc + x, 0),
+          "240": features.features.filter(f => Number(f.properties.maxStay) >= 240).map(f => Number(f.properties.length)).reduce((acc, x) => acc + x, 0),
+        //  "360": features.features.filter(f => f.properties.maxStay === '360').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
+        //  "480": features.features.filter(f => f.properties.maxStay === '480').map(f => f.properties.length).reduce((acc, x) => acc + x, 0),
       };
-
 
       const activityPieData = [
         {
@@ -368,10 +382,6 @@ class Map extends React.Component<PageProps, {}> {
           x: 'Loading',
           y: ACTIVITY_LENGTH_CALC['loading'],
         },
-        // {
-        //   x: 'Taxi Stand',
-        //   y: ACTIVITY_LENGTH_CALC['taxi stand'],
-        // },
         {
           x: 'Transit',
           y: ACTIVITY_LENGTH_CALC['transit'],
@@ -388,60 +398,57 @@ class Map extends React.Component<PageProps, {}> {
           x: 'Other Restricted Uses',
           y: ACTIVITY_LENGTH_CALC['restricted'],
         },
-        // {
-        //   x: 'Bike & scooter',
-        //   y: ACTIVITY_LENGTH_CALC['bike/scooter parking'],
-        // },
       ];
 
       const maxStayPieData = [
         {
-          x: '3 mins',
+          x: '5 min or less',
           y: MAXSTAY_LENGTH_CALC['3'],
         },
         {
-          x: '15 mins',
+          x: '15 min',
           y: MAXSTAY_LENGTH_CALC['15'],
         },
         {
-          x: '30 mins',
+          x: '30 min',
           y: MAXSTAY_LENGTH_CALC['30'],
         },
+        // {
+        //   x: '45 min',
+        //   y: MAXSTAY_LENGTH_CALC['45'],
+        // },
         {
-          x: '45 mins',
-          y: MAXSTAY_LENGTH_CALC['45'],
-        },
-        {
-          x: '60 mins',
+          x: '1 hr',
           y: MAXSTAY_LENGTH_CALC['60'],
         },
+        // {
+        //   x: '90 min',
+        //   y: MAXSTAY_LENGTH_CALC['90'],
+        // },
         {
-          x: '90 mins',
-          y: MAXSTAY_LENGTH_CALC['90'],
-        },
-        {
-          x: '120 mins',
+          x: '2 hr',
           y: MAXSTAY_LENGTH_CALC['120'],
         },
         {
-          x: '180 mins',
+          x: '3 hr',
           y: MAXSTAY_LENGTH_CALC['180'],
         },
         {
-          x: '240 mins',
-          y: ACTIVITY_LENGTH_CALC['240'],
+          x: '4 hr or more',
+          y: MAXSTAY_LENGTH_CALC['240'],
         },
-        {
-          x: '360 mins',
-          y: ACTIVITY_LENGTH_CALC['360'],
-        },
-        {
-          x: '480 mins',
-          y: ACTIVITY_LENGTH_CALC['480'],
-        },
+        // {
+        //   x: '360 mins',
+        //   y: ACTIVITY_LENGTH_CALC['360'],
+        // },
+        // {
+        //   x: '8 hr',
+        //   y: ACTIVITY_LENGTH_CALC['480'],
+        // },
       ];
 
 
+// time query below adds one minute to selected time, to reconcile conflicting regulations that begin and end at the hour mark
       return (
 
         <Layout>
@@ -468,30 +475,30 @@ class Map extends React.Component<PageProps, {}> {
           </Select>
           &nbsp; &nbsp;
           Time: <Select defaultValue={time} onChange={this.changeTime}>
-            <Select.Option value="00:00">00:00</Select.Option>
-            <Select.Option value="01:00">01:00</Select.Option>
-            <Select.Option value="02:00">02:00</Select.Option>
-            <Select.Option value="03:00">03:00</Select.Option>
-            <Select.Option value="04:00">04:00</Select.Option>
-            <Select.Option value="05:00">05:00</Select.Option>
-            <Select.Option value="06:00">06:00</Select.Option>
-            <Select.Option value="07:00">07:00</Select.Option>
-            <Select.Option value="08:00">08:00</Select.Option>
-            <Select.Option value="09:00">09:00</Select.Option>
-            <Select.Option value="10:00">10:00</Select.Option>
-            <Select.Option value="11:00">11:00</Select.Option>
-            <Select.Option value="12:00">12:00</Select.Option>
-            <Select.Option value="13:00">13:00</Select.Option>
-            <Select.Option value="14:00">14:00</Select.Option>
-            <Select.Option value="15:00">15:00</Select.Option>
-            <Select.Option value="16:00">16:00</Select.Option>
-            <Select.Option value="17:00">17:00</Select.Option>
-            <Select.Option value="18:00">18:00</Select.Option>
-            <Select.Option value="19:00">19:00</Select.Option>
-            <Select.Option value="20:00">20:00</Select.Option>
-            <Select.Option value="21:00">21:00</Select.Option>
-            <Select.Option value="22:00">22:00</Select.Option>
-            <Select.Option value="23:00">23:00</Select.Option>
+            <Select.Option value="00:01">00:00</Select.Option>
+            <Select.Option value="01:01">01:00</Select.Option>
+            <Select.Option value="02:01">02:00</Select.Option>
+            <Select.Option value="03:01">03:00</Select.Option>
+            <Select.Option value="04:01">04:00</Select.Option>
+            <Select.Option value="05:01">05:00</Select.Option>
+            <Select.Option value="06:01">06:00</Select.Option>
+            <Select.Option value="07:01">07:00</Select.Option>
+            <Select.Option value="08:01">08:00</Select.Option>
+            <Select.Option value="09:01">09:00</Select.Option>
+            <Select.Option value="10:01">10:00</Select.Option>
+            <Select.Option value="11:01">11:00</Select.Option>
+            <Select.Option value="12:01">12:00</Select.Option>
+            <Select.Option value="13:01">13:00</Select.Option>
+            <Select.Option value="14:01">14:00</Select.Option>
+            <Select.Option value="15:01">15:00</Select.Option>
+            <Select.Option value="16:01">16:00</Select.Option>
+            <Select.Option value="17:01">17:00</Select.Option>
+            <Select.Option value="18:01">18:00</Select.Option>
+            <Select.Option value="19:01">19:00</Select.Option>
+            <Select.Option value="20:01">20:00</Select.Option>
+            <Select.Option value="21:01">21:00</Select.Option>
+            <Select.Option value="22:01">22:00</Select.Option>
+            <Select.Option value="23:01">23:00</Select.Option>
           </Select>
           <br />
           <br />
@@ -502,27 +509,46 @@ class Map extends React.Component<PageProps, {}> {
           <br />
           <br />
 
-          <Pie
-            animate={false}
-            colors={Object.values(ACTIVITY_COLOR_MAP)}
-            hasLegend
-            title="Activities"
-            subTitle={<>Total car<br/>lengths</>}
-            total={() => (
-              <>
-                <span>
-                  { (activityPieData.reduce((pre, now) => now.y + pre, 0) / avgParkingLength).toLocaleString('en', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits : 0 }) }
-                </span>
-              </>
-            )}
-            data={activityPieData}
-            valueFormat={val => <span>{ (val / avgParkingLength).toLocaleString('en', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits : 0 })} cars</span>}
-            height={240}
-          />
+{ mode === "maxStay" ?
+  <Pie
+    animate={false}
+    colors={Object.values(MAXSTAY_COLOR_MAP)}
+    hasLegend
+    title="Maximum Stay"
+    subTitle={<>Total car<br/>lengths</>}
+    total={() => (
+      <>
+        <span>
+          { (maxStayPieData.reduce((pre, now) => now.y + pre, 0) / avgParkingLength).toLocaleString('en', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits : 0 }) }
+        </span>
+      </>
+    )}
+    data={maxStayPieData}
+    valueFormat={val => <span>{ (val / avgParkingLength).toLocaleString('en', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits : 0 })} cars</span>}
+    height={240}
+  />
+  :
+  <Pie
+    animate={false}
+    colors={Object.values(ACTIVITY_COLOR_MAP)}
+    hasLegend
+    title="Activities"
+    subTitle={<>Total car<br/>lengths</>}
+    total={() => (
+      <>
+        <span>
+          { (activityPieData.reduce((pre, now) => now.y + pre, 0) / avgParkingLength).toLocaleString('en', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits : 0 }) }
+        </span>
+      </>
+    )}
+    data={activityPieData}
+    valueFormat={val => <span>{ (val / avgParkingLength).toLocaleString('en', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits : 0 })} cars</span>}
+    height={240}
+  />
+
+}
 
           <br />
-
-            {legend}
 
           </Card>
         </Layout>
@@ -531,25 +557,5 @@ class Map extends React.Component<PageProps, {}> {
       );
     }
   }
-
-
-// maxStay legend
-  // <Pie
-  //   animate={false}
-  //   colors={Object.values(MAXSTAY_COLOR_MAP)}
-  //   hasLegend
-  //   title="MaxStay"
-  //   subTitle="Max Stay (mins)"
-  //   total={() => (
-  //     <>
-  //       <span>
-  //         { (maxStayPieData.reduce((pre, now) => now.y + pre, 0) / avgParkingLength).toLocaleString('en', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits : 0 }) }
-  //       </span>
-  //     </>
-  //   )}
-  //   data={maxStayPieData}
-  //   valueFormat={val => <span>{(val / avgParkingLength).toLocaleString('en', { style: 'decimal', maximumFractionDigits : 0, minimumFractionDigits : 0 })} spaces</span>}
-  //   height={240}
-  // />
 
   export default connect(mapStateToProps)(Map);
